6e7866f701ff0111619dafeae588d222fc4b97b5
astar <astarfryazino@gmail.com>
2016-08-26 00:13:00 +0300

    *

--- alta2016_main.py
+++ alta2016_main.py
@@ -4,5 +4,7 @@ import numpy as np
 pairs_train = pd.read_csv('data/alta16_kbcoref_train_pairs.csv', sep=',', index_col='Id')
 labels_train = pd.read_csv('data/alta16_kbcoref_train_labels.csv', sep=',', index_col='Id')
 data_train = pairs_train.join(labels_train, how='inner')
+data_test = pd.read_csv('data/alta16_kbcoref_test_pairs.csv', sep=',', index_col='Id')
 
 print data_train.head()
+print data_test.head()

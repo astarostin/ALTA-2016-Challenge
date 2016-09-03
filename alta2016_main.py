92313ce0f06b6e722f1770d9040d620b4a1ce50d
astar <astarfryazino@gmail.com>
2016-09-03 12:05:07 +0300

    Refactoring...

--- alta2016_main.py
+++ alta2016_main.py
@@ -1,27 +1,25 @@
-from feature_preparation import prepare_features_tfidf
-from validation import test_logistic_regression, test_logistic_regression_cv
+from feature_preparation import prepare_features
+from validation import prepare_predictor
 from prediction import predict
 import pandas as pd
 
 # load data
-pairs_train = pd.read_csv('data/alta16_kbcoref_train_pairs.csv', sep=',', index_col='Id')
-labels_train = pd.read_csv('data/alta16_kbcoref_train_labels.csv', sep=',', index_col='Id')
+# pairs_train = pd.read_csv('data/alta16_kbcoref_train_pairs.csv', sep=',', index_col='Id')
+# pairs_test = pd.read_csv('data/alta16_kbcoref_test_pairs.csv', sep=',', index_col='Id')
 search_train = pd.read_csv('data/alta16_kbcoref_train_search_results.csv', sep=',', index_col='Id')
+labels_train = pd.read_csv('data/alta16_kbcoref_train_labels.csv', sep=',', index_col='Id')
 search_test = pd.read_csv('data/alta16_kbcoref_test_search_results.csv', sep=',', index_col='Id')
-pairs_test = pd.read_csv('data/alta16_kbcoref_test_pairs.csv', sep=',', index_col='Id')
 
 # prepare features
-data_train, data_test = prepare_features_tfidf(pairs_train, pairs_test, search_train, search_test)
+# possible modes: tfidf
+data_train, data_test = prepare_features(search_train, search_test, mode='tfidf')
 
 # target data
 y = labels_train['Outcome']
 
-# cross-validation
-# predictor = test_logistic_regression(data_train, y)
-
 # grid search for best parameters
-predictor, cv_score = test_logistic_regression_cv(data_train, y)
+# possible modes: logreg
+predictor, cv_score = prepare_predictor(data_train, y, mode='logreg')
 
 # make predictions
 predict(predictor, data_train, y, data_test, cv_score)
-
